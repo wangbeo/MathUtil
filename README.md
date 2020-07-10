01be79bb83b03610930fb37618ef68820721dd42
wangbeo <beowang0987@gmail.com>
2020-07-10 17:02:20 +0700

    list: note -> the first upload

---
+++ README.md
@@ -0,0 +1,15 @@
+# Welcome to WangBeo repository
+This repository contains a simple Java application used for demonstration of CI- Continuous Intergration process,
+a part of Introduction to Software Engineering course at FPT University, HCMC Campus
+
+## You will find in this repo:
+* MathUtil class: hold all of our own math method
+* Test class: demonstrate TDD and Unit Testing using JUnit
+* .gitignore
+* Markdown file itself
+
+## Connect me via
+1. [GitHub](https://www.github.com/wangbeo)
+2. [Facebook](https://www.facebook.com/Nguyen.Quang.1712)
+
+### © 2020 by WangBeo
---
+++ src/com/quang/util/MathUtil.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.quang.util;
+
+/**
+ *
+ * @author wangp
+ */
+// class nay chua những hàm toán học, tính toán gì đó, giúp cho mọi nơi khác 
+// phàm cái gì mang tính chất xài  chung cho mọi nơi, t se dùng kĩ thuật
+// STATIC
+public class MathUtil {
+    // tinh n! = 1.2.3...n -> vì n! tăng kết quả rất nhanh, sẽ tràn kiểu int sớm, do int chỉ tối đa 2 tỷ 1, do đó xài long an toàn hơn
+    // tuy thế 15! đã là to lắm, mình hạn chế chỉ tính 15! đỏ lại
+    public static long computeFactorial(int n)
+    {
+        long result = 1;
+        for( int i=1; i<n; i++){
+            result *=i;
+            
+    }
+    return result;
+    }
+}
